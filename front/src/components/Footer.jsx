293b9bae311fdd8e934dc4b0de081211910ee268
Lam Nguyen <hoangthanhla@student.unimelb.edu.au>
2024-04-06 13:13:23 +1100

    update header

--- front/src/components/Footer.jsx
+++ front/src/components/Footer.jsx
@@ -5,8 +5,7 @@ function Footer() {
     return (
         <footer>
             <div id="contact">
-                <h3>Github repository:</h3>
-                <a href="https://github.com/artwork321/codebrew2024">Flach@Codebrew2024</a>
+                <h3>Github repository: <a href="https://github.com/artwork321/codebrew2024">Flach@Codebrew2024</a></h3>
             </div>
         </footer>
     );
--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,16 +1,19 @@
 // Navbar.js
 import React from 'react';
 import { Link } from "react-scroll";
+import '../index.css'
 
 function Navbar() {
     return (
-        <div className="navbar">
-            <a className="name" href="/">Flach</a>
-            <Link to="about">About us</Link>
-            <Link to="charities">Find a charity</Link>
-            <Link to="faqs">FAQs</Link>
-            <Link to="contact">Contact us</Link>
-        </div>
+        <header>
+            <div className='navbar'>
+                <a className="name" href="/">Flach</a>
+                <Link to="about">About us</Link>
+                <Link to="charities">Find a charity</Link>
+                <Link to="faqs">FAQs</Link>
+                <Link to="contact">Contact us</Link>
+            </div>
+        </header>
     );
 }
 
