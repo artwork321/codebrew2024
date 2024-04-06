ed3c8699774529326a5ecc10ca5a01701e04564f
NoNam312 <164467547+NoNam312@users.noreply.github.com>
2024-04-06 14:11:13 +1100

    update streamlit page

---
+++ pythonBack/dataloader.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import csv
+import requests
+from io import BytesIO
+from PIL import Image
+
+def database_loader(charity):
+    def read_csv_file(file_path):
+        try:
+            with open(file_path, 'r', newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    csv_name = row[0]
+
+                    if csv_name == charity:
+                        return [csv_name, row[1], row[2], row[3]]
+        except FileNotFoundError:
+            st.error("File not found. Please check the file path.")
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+    
+    def display_image(url):
+        response = requests.get(url)
+        if response.status_code == 200:
+            image_data = response.content
+            # Open the image using Pillow
+            image = Image.open(BytesIO(image_data))
+            # Resize the image to 100x100
+            image_resized = image.resize((200, 200))
+            # Display the image in Streamlit
+            st.image(image_resized, use_column_width=False)
+        else:
+            st.write("Failed to load image")
+
+    # Inputting File Path
+    file_path = r"crewcode2024\\pythonBack\\mainpage.csv"
+    data = read_csv_file(file_path)
+
+    if data is not None:
+        display_image(data[3])
+        st.write(data[0])
+        st.write(data[1])
+        st.write(data[2])  
+    else:
+        st.error("Charity not found in the database")
--- pythonBack/streamlitBack.py
+++ pythonBack/streamlitBack.py
@@ -5,9 +5,9 @@ from PIL import Image
 import requests
 from io import BytesIO, StringIO
 import pandas as pd
+from crewcode2024.pythonBack.dataloader import database_loader
 
 
-st.set_page_config(layout="wide") 
 
 # Load your database from a file (replace 'your_database.csv' with your actual file path)
 file_url = 'https://raw.githubusercontent.com/artwork321/crewcode2024/hedy/pages/datadotgov_ais19_editedsheet.csv'
@@ -439,7 +439,7 @@ def main():
 
     row1 = option_menu(
         menu_title      =   None,
-        options         =   ['Caritas', 'World Vision Australia', 'The Smith Family', 'RSPCA Australia'],
+        options         =   ['Salvation Army', 'Royal Flying Doctor Service of Australia', 'The Smith Family', 'RSPCA Australia'],
         menu_icon       =   None,
         default_index   =   0,
         orientation     =   'horizontal',
@@ -482,41 +482,55 @@ def main():
         
     )
 
-    if row1 == 'Caritas':
+    if row1 == 'Salvation Army':
 
         # Displaying Dataframes and Images
-        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiURz1xi1_VpLK2qwAGULMHmG1UGwRUX_DVwuTUnlOTw&s')
+        display_image('https://yt3.ggpht.com/-Zv_ePLaSilk/AAAAAAAAAAI/AAAAAAAAAAA/oosqEcbxBFc/s900-c-k-no-mo-rj-c0xffffff/photo.jpg')
 
         # Caritas Dataframe
-        name = 'Caritas'
-        purpose = 'Caritas Internationalis is a global network of Catholic charitable organizations, operating in over 200 countries. It provides aid, promotes social justice, and empowers communities in need worldwide.'
+        name = 'Salvation Army'
+        purpose = 'The Salvation Army, established in 1883, provides vital social services across Australia, including emergency aid and rehabilitation programs. It offers support to individuals and families in need, assisting with various challenges they may face.'
 
         # Display the information
         st.write(f"Name: {name}", style=custom_style)
         st.write(f"Purpose: {purpose}")
+        salvo_button = st.button('More Information')
 
-    if row1 == 'World Vision Australia':
+        if salvo_button == True:
+            database_loader('Salvation Army')
+
+    if row1 == 'Royal Flying Doctor Service of Australia':
         # Displaying Dataframes and Images
-        display_image('https://pbs.twimg.com/profile_images/1009985406069141504/c_0KvxaW_400x400.jpg')
+        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiVoqoTUsf5G73yOchBcBPLMQSPkk5M9GMI6MsNzM1VA&s')
 
         # World Vision Australia Dataframe
-        name = 'World Vision Australia'
+        name = 'Royal Flying Doctor Service of Australia'
         purpose = 'World Vision Australia is a humanitarian organization dedicated to helping children and communities in need through various programs, including child sponsorship, emergency relief, and community development.'
 
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
+        rfdsa_button = st.button('More Information')
+
+        if rfdsa_button == True:
+            database_loader('Royal Flying Doctor Service of Australia')
+
+        
 
     if row1 == 'The Smith Family':
-        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnIz-AXLJ8KIPuKuJO2wJbPQVnXipcpqY449AMghhZmw&s')
+        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR9EuEHX9ZtOIuHjxKhNDhNQ5L4kzKSnC1R1TE36pNWyw&s')
 
         # RSPCA Australia Dataframe
-        name = 'RSPCA Australia'
-        purpose = 'RSPCA Australia is an animal welfare organization in Australia. It works to prevent cruelty to animals by promoting animal welfare standards, conducting investigations, and providing shelter, adoption, and veterinary services for animals in need.'
+        name = 'The Smith Family'
+        purpose = 'The Smith Family, founded in 1922, is dedicated to helping disadvantaged children in Australia through education support and financial assistance programs. They aim to break the cycle of disadvantage by providing resources and opportunities for children and young people to thrive academically and build better futures for themselves'
 
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
+        tsf_button = st.button('More Information')
+
+        if tsf_button == True:
+            database_loader('The Smith Family')
 
     if row1 == 'RSPCA Australia':
         #Displaying Dataframes and Images
@@ -529,12 +543,16 @@ def main():
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
+        rspca_button = st.button('More Information')
+
+        if rspca_button == True:
+            database_loader('The Smith Family')
 
 
 
     row2 = option_menu(
         menu_title      =   None,
-        options         =   ['Carita', 'WVA', 'TSF', 'RSPCA'],
+        options         =   ['St. Vincent de Paul Society (Vinnies)', 'Cancer Council Australia', 'Australian Red Cross', 'Oxfam Australia'],
         menu_icon       =   None,
         default_index   =   0,
         orientation     =   'horizontal',
@@ -577,53 +595,71 @@ def main():
         
     )
 
-    if row2 == 'Carita':
+
+    if row2 == 'St. Vincent de Paul Society (Vinnies)':
 
         # Displaying Dataframes and Images
-        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiURz1xi1_VpLK2qwAGULMHmG1UGwRUX_DVwuTUnlOTw&s')
+        display_image('https://www.sydneycatholic.org/casys/wp-content/sabai/File/files/l_1fe4a4bfd7938a697171ff23a792debd.jpg')
 
         # Caritas Dataframe
-        name = 'Caritas'
-        purpose = 'Caritas Internationalis is a global network of Catholic charitable organizations, operating in over 200 countries. It provides aid, promotes social justice, and empowers communities in need worldwide.'
+        name = 'St. Vincent de Paul Society (Vinnies)'
+        purpose = 'Vinnies, a volunteer charity in Australia, provides practical aid to those in need, striving to combat poverty and foster social inclusion. Their efforts focus on offering support and empowerment to individuals and families facing various challenges.'
 
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
 
-    if row2 == 'WVA':
+        vinnies_button = st.button('More Information!!')
+
+        if vinnies_button == True:
+            database_loader('St. Vincent de Paul Society (Vinnies)')
+
+    if row2 == 'Cancer Council Australia':
         # Displaying Dataframes and Images
-        display_image('https://pbs.twimg.com/profile_images/1009985406069141504/c_0KvxaW_400x400.jpg')
+        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSyvzdyEdiAP0uckKo_5Njv_LU0ZSitfsn7aretLv57Tw&s')
 
         # World Vision Australia Dataframe
-        name = 'World Vision Australia'
-        purpose = 'World Vision Australia is a humanitarian organization dedicated to helping children and communities in need through various programs, including child sponsorship, emergency relief, and community development.'
+        name = 'Cancer Council Australia'
+        purpose = 'Cancer Council Australia is a leading national organization dedicated to reducing the impact of cancer, working extensively on research, prevention, and support services across the country. Their multifaceted approach includes funding research projects and advocating for better access to cancer care, aiming to improve outcomes for individuals and families affected by cancer.'
 
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
+        cancer_council_button = st.button('More Information!!')
 
-    if row2 == 'TSF':
-        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnIz-AXLJ8KIPuKuJO2wJbPQVnXipcpqY449AMghhZmw&s')
+        if cancer_council_button == True:
+            database_loader('Cancer Council Australia')
+
+    if row2 == 'Australian Red Cross':
+        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKvyU7gCi23fuKf2m98Fe3M-jBzjPrEIHtmLn3pocn3A&s')
 
         # RSPCA Australia Dataframe
-        name = 'RSPCA Australia'
-        purpose = 'RSPCA Australia is an animal welfare organization in Australia. It works to prevent cruelty to animals by promoting animal welfare standards, conducting investigations, and providing shelter, adoption, and veterinary services for animals in need.'
+        name = 'Australian Red Cross'
+        purpose = 'Australian Red Cross provides vital humanitarian services across Australia, offering emergency response, disaster relief, and community support, embodying compassion and assistance in times of crisis. Through their nationwide network, they deliver essential aid, demonstrating unwavering dedication to helping those in need.'
 
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
+        arc_button = st.button('More Information!!')
 
-    if row2 == 'RSPCA':
+        if arc_button == True:
+            database_loader('Australian Red Cross')
+
+    if row2 == 'Oxfam Australia':
         #Displaying Dataframes and Images
-        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnIz-AXLJ8KIPuKuJO2wJbPQVnXipcpqY449AMghhZmw&s')
+        display_image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzBHbUD4QKgz2DY1agps3cA56EpEW9nEqb0hCsldfZWA&s')
         
         #RSPCA Australia
-        name = 'RSPCA Australia'
-        purpose = 'RSPCA Australia is an animal welfare organization in Australia. It works to prevent cruelty to animals by promoting animal welfare standards, conducting investigations, and providing shelter, adoption, and veterinary services for animals in need.'
+        name = 'Oxfam Australia'
+        purpose = 'Oxfam Australia, part of the global Oxfam confederation, works to combat poverty worldwide through emergency response, sustainable development, and advocacy for social justice. They address issues like clean water access, gender equality, and education, striving to create lasting change and empower marginalized communities.'
 
         # Display the information
         st.write(f"Name: {name}")
         st.write(f"Purpose: {purpose}")
+        oxfam_button = st.button('More Information!!')
+
+        if oxfam_button == True:
+            database_loader('Oxfam Australia')
 
 
 if selected == 'Main':
