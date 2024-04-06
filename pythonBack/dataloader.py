4b0c4de12dbc7c16cda62bea848eb72af5b01fd8
Lam Nguyen <hoangthanhla@student.unimelb.edu.au>
2024-04-06 14:34:06 +1100

    add cache

--- pythonBack/dataloader.py
+++ pythonBack/dataloader.py
@@ -6,6 +6,7 @@ from PIL import Image
 
 file_url = 'https://raw.githubusercontent.com/artwork321/crewcode2024/hedy/pages/datadotgov_ais19_editedsheet.csv'
 
+@st.cache
 def database_loader(charity):
     def read_csv_file(file_path):
         try:
